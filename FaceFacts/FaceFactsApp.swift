e208f4e7dd870f6c1b221bba4239f08e6511742b
MichaelZakaria <65913937+MichaelZakaria@users.noreply.github.com>
2024-03-27 13:35:24 +0200

    Initial Commit

---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by Marco Zakaria on 27/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
